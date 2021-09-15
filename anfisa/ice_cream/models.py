70bc41cf0a4469f0cea86f0f883f626ba1fcf54b
Aleksandr Borovtsov <alexnavidu@gmail.com>
2021-09-15 21:18:35 +0300

    Create model IceCream

--- anfisa/ice_cream/models.py
+++ anfisa/ice_cream/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Объявляем класс IceCream, наследник класса Model из пакета models
+class IceCream(models.Model):
+    # Тип: CharField (строка с ограничением длины)
+    name = models.CharField(max_length=200)
+    # Тип: TextField (текстовое поле)
+    # используется для больших текстовых блоков
+    description = models.TextField()
+    # Тип: булева переменная, «да/нет»;
+    # значение по умолчанию: True
+    on_main = models.BooleanField(default=True)
